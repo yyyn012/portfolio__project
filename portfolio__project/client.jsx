b47bb04da002166caa0d480e595f31d36041df19
yyyn012 <yyyn12_@naver.com>
2024-09-10 16:36:34 +0900

    installed webpack-cli again
    modify client.jsx

--- portfolio__project/client.jsx
+++ portfolio__project/client.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import Portfolio from "./Portfolio";
+import Portfolio from "./src/Components/Portfolio";
 
 ReactDOM.createRoot(document.querySelector("#root")).render(<Portfolio />);
