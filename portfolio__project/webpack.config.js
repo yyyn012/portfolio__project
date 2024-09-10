e3e25249304300d054d1dd66d3f6a99f85c9ac02
yyyn012 <yyyn12_@naver.com>
2024-09-10 16:24:10 +0900

    webpack.config.js

---
+++ portfolio__project/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const RefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "eval",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  entry: {
+    app: ["./client"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              { targets: { browsers: ["> 1% in KR"] }, debug: true },
+            ],
+            "@babel/preset-react",
+          ],
+          plugins: ["@babel/plugin-transform-class-properties"],
+        },
+      },
+    ],
+  },
+  plugins: [
+    // new webpack.HotModuleReplacementPlugin(),
+    new RefreshWebpackPlugin(),
+  ],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.js",
+    publicPath: "/dist/",
+  },
+  devServer: {
+    devMiddleware: { publicPath: "/dist" },
+    static: { directory: path.resolve(__dirname) },
+  },
+};
