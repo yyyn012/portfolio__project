9353148400631bafcc7422945cd5e54699fbc31f
yyyn012 <yyyn12_@naver.com>
2024-11-06 20:23:42 +0900

    font

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function App() {
-  return <div className="App">APP</div>;
+  return (
+    <div className="App">
+      APP
+      <h1>
+        text
+        <br />
+        테스트
+      </h1>
+    </div>
+  );
 }
 
 export default App;
