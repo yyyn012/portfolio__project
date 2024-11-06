f224604d69e3b373379e2c1400703535a35b5a66
yyyn012 <yyyn12_@naver.com>
2024-11-06 17:30:01 +0900

    install

--- portfolio__project/client.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-import Portfolio from "./src/Components/Portfolio";
-
-ReactDOM.createRoot(document.querySelector("#root")).render(<Portfolio />);
--- portfolio__project/src/Components/AboutMe.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-
-const AboutMe = () => {
-  return <div>AboutMe</div>;
-};
-
-export default AboutMe;
