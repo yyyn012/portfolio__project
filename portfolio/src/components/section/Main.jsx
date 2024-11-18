63c26b560b559c0f72b2b42afcb2bbd00f88cb4f
yyyn012 <yyyn12_@naver.com>
2024-11-18 17:03:53 +0900

    Update header.scss & Remove Footer

--- portfolio/src/components/section/Main.jsx
+++ portfolio/src/components/section/Main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import Header from "./Header";
-import Footer from "./Footer";
 
 const Main = (props) => {
   return (
@@ -10,7 +9,6 @@ const Main = (props) => {
       <main id="main" role="main">
         {props.children}
       </main>
-      <Footer />
     </div>
   );
 };
