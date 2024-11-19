bbf7fb032588fad72a747c210d2d79408840d869
yyyn012 <yyyn12_@naver.com>
2024-11-19 15:16:29 +0900

    Update aboutme

--- portfolio/src/components/contents/AboutMe.jsx
+++ portfolio/src/components/contents/AboutMe.jsx
@@ -5,14 +5,19 @@ import profile from "../../assets/img/profile.jpg";
 const AboutMe = () => {
   return (
     <section id="aboutme">
-      <img src={profile} alt="profile_img" />
-      <div className="txt_aboutme">
-        <h1>Lorem ipsum</h1>
-        <span>
-          dolor sit, amet consectetur adipisicing elit. Quis eligendi dicta
-          culpa? Blanditiis porro ea ipsa magnam laboriosam aut delectus iste,
-          sequi eveniet quia iusto sunt soluta neque, accusamus excepturi.
-        </span>
+      <h1 className="subject__txt">
+        <i class="fa-solid fa-asterisk"></i>AboutMe
+      </h1>
+      <div className="aboutme__wrap">
+        <img src={profile} alt="profile_img" />
+        <div className="txt_aboutme">
+          <h1>Lorem ipsum</h1>
+          <span>
+            dolor sit, amet consectetur adipisicing elit. Quis eligendi dicta
+            culpa? Blanditiis porro ea ipsa magnam laboriosam aut delectus iste,
+            sequi eveniet quia iusto sunt soluta neque, accusamus excepturi.
+          </span>
+        </div>
       </div>
     </section>
   );
