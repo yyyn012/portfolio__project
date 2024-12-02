28706b583cc5730d421dfe5cd91342900ea32302
yyyn012 <yyyn12_@naver.com>
2024-12-02 15:46:07 +0900

    Update Contact.jsx

--- portfolio/src/components/contents/Contact.jsx
+++ portfolio/src/components/contents/Contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
@@ -8,12 +9,11 @@ const Contact = () => {
       </h1>
 
       <div className="contact__wrap">
-        <form action="">
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <button type="button"></button>
-        </form>
+        <span>
+          github :
+          <a href="https://github.com/yyyn012"> https://github.com/yyyn012</a>
+        </span>
+        <span>e-main : yyyn12_@naver.com</span>
       </div>
     </div>
   );
