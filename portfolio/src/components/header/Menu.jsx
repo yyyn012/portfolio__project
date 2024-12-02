e9d55660c8156c252f13d7c23880a04a445f355c
yyyn012 <yyyn12_@naver.com>
2024-12-02 15:29:04 +0900

    loaction.pathname

--- portfolio/src/components/header/Menu.jsx
+++ portfolio/src/components/header/Menu.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { headerMenu } from "../../data/header";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Menu = () => {
+  const location = useLocation(0);
   return (
     <nav className="header__menu">
       <ul className="menu">
         {headerMenu.map((menu, key) => (
-          <li key={key}>
+          <li
+            key={key}
+            className={location.pathname === menu.src ? "active" : ""}
+          >
             <Link to={menu.src}>{menu.title}</Link>
           </li>
         ))}
