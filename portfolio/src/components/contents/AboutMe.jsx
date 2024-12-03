553763f45321cbbcabb4deb2b8be51a0b4c139ee
yyyn012 <yyyn12_@naver.com>
2024-12-03 20:45:31 +0900

    AboutMe(responsive)

--- portfolio/src/components/contents/AboutMe.jsx
+++ portfolio/src/components/contents/AboutMe.jsx
@@ -5,18 +5,21 @@ import profile from "../../assets/img/profile.jpg";
 const AboutMe = () => {
   return (
     <section id="aboutme">
-      <h1 className="subject__txt">
-        <i className="fa-solid fa-asterisk"></i>AboutMe
-      </h1>
       <div className="aboutme__wrap">
-        <img src={profile} alt="profile_img" />
-        <div className="aboutme__txt">
-          <h1>Lorem ipsum</h1>
-          <span>
-            dolor sit, amet consectetur adipisicing elit. Quis eligendi dicta
-            culpa? Blanditiis porro ea ipsa magnam laboriosam aut delectus iste,
-            sequi eveniet quia iusto sunt soluta neque, accusamus excepturi.
-          </span>
+        <h1 className="subject__txt">
+          <i className="fa-solid fa-asterisk"></i>AboutMe
+        </h1>
+        <div className="aboutme__content">
+          <img src={profile} alt="profile_img" />
+          <div className="aboutme__txt">
+            <h1>Lorem ipsum</h1>
+            <span>
+              dolor sit, amet consectetur adipisicing elit. Quis eligendi dicta
+              culpa? Blanditiis porro ea ipsa magnam laboriosam aut delectus
+              iste, sequi eveniet quia iusto sunt soluta neque, accusamus
+              excepturi.
+            </span>
+          </div>
         </div>
       </div>
     </section>
