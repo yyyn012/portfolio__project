36f6afa75aeaf2036e5fd4c5b34c3195e47fe7ee
yyyn012 <yyyn12_@naver.com>
2024-12-10 21:37:33 +0900

    Update Aboutme

--- portfolio/src/components/contents/AboutMe.jsx
+++ portfolio/src/components/contents/AboutMe.jsx
@@ -12,16 +12,27 @@ const AboutMe = () => {
             <i className="fa-solid fa-asterisk"></i>AboutMe
           </h1>
           <span>
-            안녕하세요. 항상 발전하는 개발자가 되고 싶은 이혜연입니다.
+            안녕하세요.<br></br> 항상 발전하는 개발자가 되고 싶은 이혜연입니다.
           </span>
           <div className="skill">
             <h1>skill</h1>
+            <hr />
             <ul className="skill__ul">
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>REACT</li>
-              <li>JAVASCRIPT</li>
-              <li>GIT</li>
+              <li>
+                <span>HTML</span>
+              </li>
+              <li>
+                <span>CSS</span>
+              </li>
+              <li>
+                <span>REACT</span>
+              </li>
+              <li>
+                <span>JAVASCRIPT</span>
+              </li>
+              <li>
+                <span>GIT</span>
+              </li>
             </ul>
           </div>
         </div>
