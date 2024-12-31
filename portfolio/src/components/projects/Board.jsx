55aefd5b04f1047525bdc4b410aacc3e7d47e248
yyyn012 <yyyn12_@naver.com>
2024-12-31 16:27:07 +0900

    Update & Add project img (2)

--- portfolio/src/components/projects/Board.jsx
+++ portfolio/src/components/projects/Board.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import board from "../../assets/img/board.png";
+
 const Board = () => {
   return (
     <div className="project__content">
       <span>Board</span>
+      <img className="project__img" src={board} alt="board__project" />
     </div>
   );
 };
